4aaa0165ae72bd785536a7ff6e73dfd6bdcc627b
Evgenius2020 <zavarzinevg@gmail.com>
2020-01-31 10:32:39 +0700

    Fixed plots indices

--- import_csv.m
+++ import_csv.m
@@ -3,5 +3,5 @@ function EEG = import_csv(datafname, locsfname, weightsfname)
     EEG = pop_importdata('data', data, 'nbchan', 32, 'srate', 512);
     EEG.chanlocs = readlocs(locsfname, 'importmode', 'eeglab', 'filetype', 'sfp');
     EEG.icawinv = csvread(weightsfname).';
-    pop_selectcomps(EEG, 1:31);
+    pop_selectcomps(EEG, 1:32);
 end
