3544aa24d71ce552b4c487babdb9183bd4f0f285
Evgenius2020 <zavarzinevg@gmail.com>
2020-02-02 20:56:18 +0700

    Remove less files

--- fif2mat.py
+++
@@ -1,8 +0,0 @@
-import mne
-from scipy import io
-fif_filepath="rudych_28_05_2018_tomoseg_3raw.fif"
-raw = mne.io.Raw(fif_filepath, preload=True, verbose=True)
-data, time = raw[:, :]
-data = data * 1000000
-io.savemat(fif_filepath[:-5], dict(data=data), oned_as='row')
-
